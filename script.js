ab61e7e6b9d8606278bf984a268ef8d8dd488f90
Oluwatobi Akinseye <tobi.akinseye@gmail.com>
2020-04-09 21:32:40 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const googleDatabase = [
+    'cats.com',
+    'souprecipeis.com',
+    'flowers.com',
+    'animals.com',
+    'catpictures.com',
+    'myfavourtitecats.com'
+]
+
+const googleSearch = (searchTerm, db) => {
+       const matches = db.filter(item =>
+         {
+             return item.includes(searchTerm)
+         }
+     )
+
+     return matches.length > 3 ? matches.slice(0, 3) : matches;
+}
+
+console.log(googleSearch('cats', googleDatabase));
